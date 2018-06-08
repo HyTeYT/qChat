9489130721ba385512f015735f13330772646ef7
Marvin Borner <marvin-borner@live.com>
2018-06-08 20:31:22 +0200

    Fixed message receiving

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,33 @@
-// Global declarations
-var ChatConnection = new WebSocket('ws://localhost:8080');
+$(function () {
+    // Global declarations
+    var ChatConnection = new WebSocket('ws://localhost:8080');
 
-ChatConnection.onOpen = function () {
-    console.log("Connection established!");
-};
+    ChatConnection.addEventListener('open', function(e){
+        console.log("Connection established!");
+    });
 
-ChatConnection.onMessage = function (e) {
-    console.log("Got message!");
-    if (Username !== undefined) {
-        const AnswerObject = JSON.parse(e.data);
-        $(".ChatMessages").append(AnswerObject.Username + " - " + AnswerObject.Message); // TODO: Write to site etc
+    ChatConnection.addEventListener('message', function(e){
+        if (Username !== undefined) {
+            const AnswerObject = JSON.parse(e.data);
+            $(".ChatMessages").append(AnswerObject.Username + " - " + AnswerObject.Message); // TODO: Write to site etc
+        }
+    });
+
+    const Username = prompt('Bitte gib deinen Namen ein!'); // TODO: Use prompt alternative
+    if (Username !== "") {
+        ChatConnection.send(JSON.stringify({
+            Type: "Join",
+            Username: Username
+        }));
+
+        OpenMessageSender(); // TODO: REWRITE!
     }
-};
 
-const Username = prompt('Bitte gib deinen Namen ein!'); // TODO: Use prompt alternative
-if (Username !== "") {
-    ChatConnection.send(JSON.stringify({
-        Type: "Join",
-        Username: Username
-    }));
-
-    OpenMessageSender(); // TODO: REWRITE!
-}
-
-function OpenMessageSender() {
-    let Message = prompt("Gebe deine Nachricht ein!"); // TODO: Interface (jquery input or sth)
-    ChatConnection.send(JSON.stringify({
-        Type: "Message",
-        Message: Message
-    }));
-}
+    function OpenMessageSender() {
+        let Message = prompt("Gebe deine Nachricht ein!"); // TODO: Interface (jquery input or sth)
+        ChatConnection.send(JSON.stringify({
+            Type: "Message",
+            Message: Message
+        }));
+    }
+});
