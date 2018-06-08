4308b5e6366eaa4aee6cc84eac3901e8e5309c56
Marco Alexander Fischer <mail@marcoalexanderfischer.de>
2018-06-08 18:15:56 +0200

    added titles

--- chat.php
+++ chat.php
@@ -2,10 +2,14 @@
 <html>
 <head>
     <link rel="stylesheet" type="text/css" href="style.css">
+    <title>qChat!</title>
     <?php $user = $_GET['u']; ?>
 </head>
 <body>
 <h1>Hi, <?php echo $user; ?>!</h1>
+    <div class="ChatContainer">
+        <form>
 
+        </form>
 </body>
 </html>
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <link rel="stylesheet" href="style.css">
+    <title>qChat!</title>
     <script>
         window.onload=function()
         {
