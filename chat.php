32223e52cc2b2491296b5b9e5a89fe53edc74972
Marco Alexander Fischer <mail@marcoalexanderfischer.de>
2018-06-08 17:53:42 +0200

    started project

---
+++ chat.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <?php $user = $_GET['u']; ?>
+</head>
+<body>
+<h1>Hi, <?php echo $user; ?>!</h1>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+    <script>
+        window.onload=function()
+        {
+                var nameinput=prompt('Bitte gib deinen Namen ein!');
+                var $user;
+                if(nameinput != "") {
+                    $user = nameinput;
+                    window.location.href = './chat.php/?u='+$user;
+                }
+                else location.reload();
+        }
+    </script>
+</head>
+<body>
+<br>
+</body>
+</html>
